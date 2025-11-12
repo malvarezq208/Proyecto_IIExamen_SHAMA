d29751ccdd5ac5543aed839eed6670ad6639447d
malvarezq208 <304860392@cuc.cr>
2025-11-12 16:51:52 -0600

    Feature:Jugador
    Avance clase jugador

--- src/clases/jugador.py
+++ src/clases/jugador.py
@@ -1,2 +1,15 @@
-class Jugador:
-    pass
+class jugador:
+    def __init__(self,player="",team="",camisa=0,nation="",position="",age=0):  #Definicion constructor
+        self.__player=player
+        self.__team=team
+        self.__camisa=camisa
+        self.__nation= nation
+        self.position=position
+        self.__age=age
+    
+    def __str__(self):
+        return f"Player:{self.__player}\n Team:{self.__team} \n Camisa:{self.__camisa} \n Nation:{self.__nation},\n Position:{self.position}\n Age:{self.__age}"
+    
+
+
+        
